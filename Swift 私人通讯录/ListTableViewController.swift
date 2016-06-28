4fa0b8a3e78ea8037a4f1f418d1ef39e55293221
刘宏立 <liuhongli2120@163.com>
2016-06-28 10:12:01 +0800

    搭建界面

---
+++ Swift 私人通讯录/ListTableViewController.swift
@@ -0,0 +1,23 @@
+//
+//  ListTableViewController.swift
+//  Swift 私人通讯录
+//
+//  Created by 刘宏立 on 16/6/28.
+//  Copyright © 2016年 lhl. All rights reserved.
+//
+
+import UIKit
+
+class ListTableViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+    }
+
+
+
+
+
+}

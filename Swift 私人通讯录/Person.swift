1ce25b5f8861048a0daa2d527f334ad86d1cdedb
刘宏立 <liuhongli2120@163.com>
2016-06-28 10:24:34 +0800

    定义联系人模型

---
+++ Swift 私人通讯录/Person.swift
@@ -0,0 +1,17 @@
+//
+//  Person.swift
+//  Swift 私人通讯录
+//
+//  Created by 刘宏立 on 16/6/28.
+//  Copyright © 2016年 lhl. All rights reserved.
+//
+
+import UIKit
+
+class Person: NSObject {
+    //定义模型,准备加载数据
+    var name: String?
+    var phone: String?
+    var title: String?
+
+}
